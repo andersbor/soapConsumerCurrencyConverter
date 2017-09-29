4a8b7479e6b7273e2492ae85326429eac3e032a6
andersbor <anbo@easj.dk>
2017-09-29 11:31:47 +0200

    cleaning

--- SoapConsumerCurrencyConverter/Program.cs
+++ SoapConsumerCurrencyConverter/Program.cs
@@ -5,6 +5,7 @@ namespace SoapConsumerCurrencyConverter
 {
     class Program
     {
+        // I added a service reference to the WSDL file of the service provider
         static void Main(string[] args)
         {
             string endpoint = "BasicHttpBinding_ICurrencyService"; // from the App.config file
@@ -16,9 +17,11 @@ namespace SoapConsumerCurrencyConverter
                 Console.WriteLine(rate);
 
                 Currency[] allCurrencies = client.GetAllCurrencyInfos();
-                foreach (var curr in allCurrencies)
+                foreach (Currency currency in allCurrencies)
                 {
-                    Console.WriteLine(curr.Code + " " + curr.Name);
+                    Console.WriteLine(currency.Code + " " + currency.Name);
+                    // Methods are not transported by SOAP from the provider to the consumer
+                    // so no ToString method on Currency objects
                 }
             }
         }
