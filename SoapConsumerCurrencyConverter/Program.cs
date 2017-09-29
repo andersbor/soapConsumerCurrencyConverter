f3c5fedd9069317dc0bf1a167723d8a6fc7006dc
andersbor <anbo@easj.dk>
2017-09-29 11:29:21 +0200

    first commit

--- SoapConsumerCurrencyConverter/Program.cs
+++ SoapConsumerCurrencyConverter/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SoapConsumerCurrencyConverter.ServiceReference1;
 
 namespace SoapConsumerCurrencyConverter
 {
@@ -10,6 +7,20 @@ namespace SoapConsumerCurrencyConverter
     {
         static void Main(string[] args)
         {
+            string endpoint = "BasicHttpBinding_ICurrencyService"; // from the App.config file
+            // This service has more than one endpoint (http + https)
+            // You must specify which endpoint to use
+            using (CurrencyServiceClient client = new CurrencyServiceClient(endpoint))
+            {
+                double rate = client.GetRate(CurrencyCode.EUR, CurrencyCode.DKK);
+                Console.WriteLine(rate);
+
+                Currency[] allCurrencies = client.GetAllCurrencyInfos();
+                foreach (var curr in allCurrencies)
+                {
+                    Console.WriteLine(curr.Code + " " + curr.Name);
+                }
+            }
         }
     }
 }
